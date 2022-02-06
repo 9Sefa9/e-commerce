4c971cb575403c8e0a0062e3d1214b9b1694028c
9Sefa9 <sefagoevercin@hotmail.de>
2022-02-06 03:03:07 +0100

    changed Navbar to react-bootstrap in order to *learn* about bootstrap

--- src/features/App/App.js
+++ src/features/App/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React,{ Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavigationBar from "../NavigationBar/NavigationBar";
--- src/features/NavigationBar/NavigationBar.js
+++ src/features/NavigationBar/NavigationBar.js
@@ -1,5 +1,39 @@
+import "./NavigationbarStylesheet.css";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+
+const NavigationBar = () => {
+  return (
+    <>
+      <Navbar className="navigation-container" collapseOnSelect expand="md" variant="light" fixed="top">
+        <Container>
+        <Navbar.Brand className="navigation-logo"href="/">Logo</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto" defaultActiveKey="/home">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <div className="shopping-cart-container">
+              <Nav.Link href="/cart"><AiOutlineShoppingCart size={35} color="black" /><span className="shopping-cart-count">999999</span></Nav.Link>  
+            </div>  
+            <NavDropdown title="User" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="/login">Login</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="/register">Register</NavDropdown.Item>
+            </NavDropdown>
+            
+          </Nav>
+        </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+export default NavigationBar;
+
+/*
 import {
-  Nav,
+ // Nav,
   NavLogo,
   NavLink,
   ImgLink,
@@ -10,10 +44,8 @@ import {
 //import shoppingCartImage from "./images/shopping-cart.png";
 import { FaRegUserCircle } from "react-icons/fa";
 import { AiOutlineShoppingCart } from "react-icons/ai";
-const NavigationBar = () => {
-  return (
-    <div>
-      <Nav>
+
+<Nav>
         <NavLogo to="/">Logo</NavLogo>
         <Bars />
         <NavMenu>
@@ -29,27 +61,9 @@ const NavigationBar = () => {
               <p className="shopping-cart-count">9999</p>
             </div>
           </ImgLink>
-          {/*<ImgLink to="/cart" activeStyle>
-            <div className="shopping-cart-container">
-              <img
-                className="shopping-cart-image"
-                src={shoppingCartImage}
-                alt="Shopping Cart Icon"
-              />
-              <p className="shopping-cart-count">9999</p>
-            </div>
-          </ImgLink>*/}
-
           <NavBtn>
-            {/*<NavBtnLink to="/sign-in" activeStyle>
-                  Sign In
-                </NavBtnLink>
-              <NavBtnLink to="/sign-up">Sign Up</NavBtnLink>*/}
             <FaRegUserCircle size={55} color="white" />
           </NavBtn>
         </NavMenu>
       </Nav>
-    </div>
-  );
-};
-export default NavigationBar;
+*/
