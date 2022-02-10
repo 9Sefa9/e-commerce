22b9463c8bb2856ed162d181a209b16dfaf3c5fd
9Sefa9 <sefagoevercin@hotmail.de>
2022-02-10 14:38:39 +0100

    changed the CatalogItem Component  a bit

--- src/features/CatalogItem/CatalogItem.js
+++ src/features/CatalogItem/CatalogItem.js
@@ -1,7 +1,11 @@
 import "./CatalogItemStylesheet.css";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import {RiPriceTag3Line} from "react-icons/ri";
-import { Badge } from "react-bootstrap";
+import {MdOutlineLocalShipping} from "react-icons/md";
+import {GiReturnArrow,GiRolledCloth,GiClothes} from "react-icons/gi";
+import {BiBarcode} from "react-icons/bi";
+import {CgColorBucket} from "react-icons/cg";
+import { InputGroup, ListGroup, Badge, DropdownButton, Dropdown } from "react-bootstrap";
 function CatalogItem(props) {
 
   return (
@@ -16,7 +20,33 @@ function CatalogItem(props) {
         </div>
         <div className="catalog-button-container">
           <button className="catalog-item-cart-button"><AiOutlineShoppingCart size={25}/>Add to cart</button>
-          <Badge pill bg="light" className="catalog-item-price"><RiPriceTag3Line size={25}/>{props.item.price}</Badge>
+          <Badge pill bg="light" className="catalog-item-price"><RiPriceTag3Line size={25}/>{props.item.price} VAT included</Badge>
+
+        </div>
+        <div>
+          <ListGroup>
+            <ListGroup.Item> 
+              <InputGroup>
+              <DropdownButton variant="outline-secondary" title={<div><CgColorBucket size={25}/>Colors</div>} id="input-group-dropdown-1">
+                <Dropdown.Item>S</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item>M</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item>L</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item >XL</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item >XXL</Dropdown.Item>
+                <Dropdown.Divider />
+              </DropdownButton>
+              </InputGroup>
+            </ListGroup.Item>
+            <ListGroup.Item><BiBarcode size={25}/>Productcode: 114955662</ListGroup.Item>
+            <ListGroup.Item><GiClothes size={25}/>About Cloth: Made of fine, elastic mesh fabric which is soft and transparent</ListGroup.Item>
+            <ListGroup.Item><GiRolledCloth size={25}/>Main: 95% Polyester, 5% Elastan.</ListGroup.Item>
+            <ListGroup.Item><MdOutlineLocalShipping  size={25}/>Free Delivery</ListGroup.Item>
+            <ListGroup.Item><GiReturnArrow size={25}/>Free Returns</ListGroup.Item>
+          </ListGroup>
         </div>
       </div>
     </article>
